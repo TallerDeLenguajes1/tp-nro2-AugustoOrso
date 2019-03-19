363c5d7fc8b635b34082991777324865baa3c520
Orsoç <augustorso94@gmail.com>
2019-03-19 16:11:20 -0300

    subindexado

---
+++ tp-nro2-AugustoOrso/ejercicio2.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void main()
+{
+	int Buff[5] = {5,15,30,10,35};
+	int *p;
+	
+	int i;
+	p = Buff;
+
+	for (i = 0; i < 5; i++)
+		{
+		printf("%d\n",Buff[i]);
+		}
+	printf("\n");
+
+	for (i = 0; i < 5; i++)
+		{
+		printf("%d\n",Buff + i);	
+		}	
+
+}
