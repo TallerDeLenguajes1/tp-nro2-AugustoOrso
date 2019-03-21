bb67f68f5907da59f756137b2fefc21f09043a39
AgussRomero22 <agustinromerojg22@gmail.com>
2019-03-21 18:14:46 -0300

    en opcion 2

---
+++ tp-nro2-AugustoOrso/tp2_1_2.cpp
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void main(){
+
+	#define N 4
+	#define M 5
+
+	int f;
+	int c;
+	double mt[N][M];
+
+	for (f = 0; f < N; f++){
+		for(c = 0; c < M; c++){
+			printf("%1f   ", mt[f][c]);
+		}
+	printf("\n");	
+	}
+// lalalala
+}
