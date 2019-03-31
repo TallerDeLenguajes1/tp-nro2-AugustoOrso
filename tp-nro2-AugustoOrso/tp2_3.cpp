a57c8b6d4adf51d15a4ae3fde3caf23226cd0e0b
Augusto <you@example.com>
2019-03-31 20:49:45 -0300

    Punto 3

---
+++ tp-nro2-AugustoOrso/tp2_3.cpp
@@ -0,0 +1,37 @@
+#include <time.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+#define N 15
+
+int main(void){
+	int f;
+	int c;
+	int cont;
+	int** mt;
+	int * arreg = (int*) malloc(sizeof(int)*15);
+
+	mt = (int **) malloc(sizeof(int*)*N);
+
+	srand(time(NULL));
+
+	for (f = 0; f < N; ++f){
+		cont = 0;
+		int num= rand() % 11 + 5;
+		mt[f] = (int *) malloc(sizeof(int) * (num));
+		for (c = 0; c < num; ++c){
+			mt[f][c] = rand() % 900 + 100;
+			printf("%d  ", mt[f][c]);
+			if ((mt[f][c]%2)==0){
+				cont++;
+			}
+		}
+		arreg[f] = cont;
+	}
+
+	printf("\nCantidad de pares por fila:");
+
+	for (int i = 0; i < N; ++i){
+		printf("%d  ", arreg[i]);
+	}
+}
